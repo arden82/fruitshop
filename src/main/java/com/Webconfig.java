1e1cacee237df2d4a237796ed3ecb87009e062c4
arden <qinwen1018@gmail.com>
2023-12-14 12:41:09 +0800

    2-1

---
+++ src/main/java/com/Webconfig.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Webconfig implements WebMvcConfigurer{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
